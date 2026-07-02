327198920324330a7af9dd35c2ef4f6ebdff6fcb
Koshi <mikazejnalov@gmail.com>
2026-07-02 21:56:28 +0300

    chore: deploy to Vercel

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,10 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+console.log('🔑 API Key:', import.meta.env.VITE_FIREBASE_API_KEY);
+console.log('🌐 Auth Domain:', import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
+console.log('📦 Project ID:', import.meta.env.VITE_FIREBASE_PROJECT_ID);
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
